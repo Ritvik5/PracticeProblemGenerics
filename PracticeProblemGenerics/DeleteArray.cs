eaff8157b0c04752187bd11f84083b9fe1df03b8
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-16 17:29:29 +0530

    [Ritvik]Add: Using Generic class

--- PracticeProblemGenerics/DeleteArray.cs
+++ PracticeProblemGenerics/DeleteArray.cs
@@ -3,57 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PracticeProblemGenerics
 {
-    public class DeleteArray
+    public class DeleteArray<T>
     {
-        public void DeleteElement(object[] array, object element)
-        {
+        //public void DeleteElement(object[] array, object element)
+        //{
 
-            for(int i = 0; i < array.Length; i++)
-            {
-                if (array[i].Equals(element))
-                {
-                    for(int j = i; j < array.Length -1 ; j++)
-                    {
-                        array[j] = array[j + 1];
-                    }
-                    // Setting the last value null
-                    array[array.Length - 1] = null;
-                    break;
-                }
-            }
+        //    for(int i = 0; i < array.Length; i++)
+        //    {
+        //        if (array[i].Equals(element))
+        //        {
+        //            for(int j = i; j < array.Length -1 ; j++)
+        //            {
+        //                array[j] = array[j + 1];
+        //            }
+        //            // Setting the last value null
+        //            array[array.Length - 1] = null;
+        //            break;
+        //        }
+        //    }
 
-            foreach (object obj in array)
-            {
-                Console.Write(obj + " ");
-            }
-            Console.WriteLine();
-        }
+        //    foreach (object obj in array)
+        //    {
+        //        Console.Write(obj + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
 
-        public void DeleteMethod<T>(ref T[] array1, T elements)
+        //public void DeleteMethod<T>(ref T[] array1, T elements)
+        //{
+        //    int count = 0;
+
+        //    for(int i = 0; i < array1.Length; i++)
+        //    {
+        //        if (array1[i].Equals(elements))
+        //        {
+        //            count++;
+        //        }
+        //        else if (count > 0)
+        //        {
+        //            array1[i-count] = array1[i];
+        //        }
+        //    }
+        //    Array.Resize(ref array1,array1.Length - count);
+
+        //    foreach (T obj1 in array1)
+        //    {
+        //        Console.Write(obj1 + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        public void DeleteMethod(T[] array, T element)
         {
             int count = 0;
-
-            for(int i = 0; i < array1.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                if (array1[i].Equals(elements))
+                if (array[i].Equals(element))
                 {
                     count++;
                 }
                 else if (count > 0)
                 {
-                    array1[i-count] = array1[i];
+                    array[i - count] = array[i];
                 }
             }
-            Array.Resize(ref array1,array1.Length - count);
-
-            foreach (T obj1 in array1)
+            Array.Resize(ref array, array.Length - count);
+            foreach (T elements in array)
             {
-                Console.Write(obj1 + " ");
+                Console.Write(elements + " ");
             }
-            Console.WriteLine();
         }
+
+            
     }
 }
+
+
--- PracticeProblemGenerics/Program.cs
+++ PracticeProblemGenerics/Program.cs
@@ -7,15 +7,32 @@
             Console.WriteLine("Welcome to Generic array deletion!!!\n");
 
             //-- Using Object array non generic method
-            DeleteArray array = new DeleteArray();
+            //DeleteArray array = new DeleteArray();
             //object[] arr = { 10, 20, 30.14, 5, 'A', 2.84 };
             //array.DeleteElement(arr, 5);
 
             //-- Using generic method
 
-            int[] arr1 = {10,20,30,40,50};
-            array.DeleteMethod(ref arr1, 40);
+            //int[] arr1 = {10,20,30,40,50};
+            //array.DeleteMethod(ref arr1, 40);
             
+
+            //-- Using Generic class 
+
+            DeleteArray<int> array = new DeleteArray<int>();
+
+            int[] arr = { 10, 20, 30, 31, 32, };
+
+            array.DeleteMethod(arr, 32);
+
+            Console.WriteLine("\n:::::::::::::::::::::::::::::\n");
+
+
+            DeleteArray<double> array2 = new DeleteArray<double>();
+            double[] arr2 = { 1.14,2.15,3.15,3.33,4.50 };
+
+            array2.DeleteMethod(arr2, 3.15);
+            Console.Read();
         }
     }
 }
