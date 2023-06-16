a12ef4466d98a34b153886c339f5eb2e1d4bf8d9
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-16 16:21:40 +0530

    [Ritvik]Add: Object array is used to delete the element

---
+++ PracticeProblemGenerics/DeleteArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemGenerics
+{
+    public class DeleteArray
+    {
+        public void DeleteElement(object[] array, object element)
+        {
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(element))
+                {
+                    for(int j = i; j < array.Length -1 ; j++)
+                    {
+                        array[j] = array[j + 1];
+                    }
+                    // Setting the last value null
+                    array[array.Length - 1] = null;
+                    break;
+                }
+            }
+
+            foreach (object obj in array)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- PracticeProblemGenerics/Program.cs
+++ PracticeProblemGenerics/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generic array deletion!!!");
+            Console.WriteLine("Welcome to Generic array deletion!!!\n");
+
+            DeleteArray array = new DeleteArray();
+
+
+            object[] arr = { 10, 20, 30.14, 5, 'A', 2.84 };
+
+            array.DeleteElement(arr, 5);
         }
     }
 }
