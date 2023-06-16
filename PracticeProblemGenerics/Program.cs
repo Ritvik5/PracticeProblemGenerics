d548f8c78293ad0eea07f30da10646f72cb3c668
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-16 15:55:01 +0530

    [Ritvik]Add: Welcome Message

---
+++ PracticeProblemGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticeProblemGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic array deletion!!!");
+        }
+    }
+}
